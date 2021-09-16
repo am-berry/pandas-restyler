ab4ab35be7972c01fc061ed91717980fcb35e8fd
am-berry <aman.berry725@gmail.com>
2021-09-16 13:57:03 +0100

    Make pagination work with styles

--- pandas_paginate.py
+++ pandas_paginate.py
@@ -1,52 +1,50 @@
 from IPython.core.display import display, HTML, clear_output
 from ipywidgets import widgets
-from pandas.api.extensions import (
-    register_dataframe_accessor, register_series_accessor
-)
+from pandas.api.extensions import register_dataframe_accessor, register_series_accessor
 import math
 
-__version__ = '0.1.0'
+__version__ = "0.1.0"
 
 
 class Paginator:
-
-    def __init__(self, df, pagesize=15, start_page=0):
+    def __init__(self, df, tooltip=None, pagesize=15, start_page=0):
         self.df = df
+        self.tooltip = tooltip
         self.pagesize = pagesize
         self.page = start_page
         self.max_pages = math.ceil(df.shape[0] / self.pagesize)
 
-        self.btn_next = widgets.Button(description='Next')
+        self.btn_next = widgets.Button(description="Next")
         self.btn_next.on_click(self._next_page)
 
-        self.btn_prev = widgets.Button(description='Prev')
+        self.btn_prev = widgets.Button(description="Prev")
         self.btn_prev.on_click(self._prev_page)
 
-        self.label = widgets.Label(
-            'PAGE {}/{}'.format(self.page + 1, self.max_pages)
-        )
+        self.label = widgets.Label("PAGE {}/{}".format(self.page + 1, self.max_pages))
 
         self.slider = widgets.IntSlider(
             value=1,
             max=self.max_pages,
-            min=start_page+1,
+            min=start_page + 1,
             step=1,
         )
-        self.slider.observe(self._handle_slider_change, names='value')
+        self.slider.observe(self._handle_slider_change, names="value")
 
         self._styles = []
 
-        self.control = widgets.HBox((
-            self.btn_prev,
-            self.label,
-            self.btn_next,
-            self.slider,
-        ))
+        self.control = widgets.HBox(
+            (
+                self.btn_prev,
+                self.label,
+                self.btn_next,
+                self.slider,
+            )
+        )
         self.content = widgets.HTML(self._render_table(self.get_page()))
 
     def get_page(self):
         return self.df.iloc[
-            self.page * self.pagesize: (self.page + 1) * self.pagesize
+            self.page * self.pagesize : (self.page + 1) * self.pagesize
         ]
 
     @property
@@ -81,12 +79,20 @@ class Paginator:
 
     def _render_table(self, df):
         try:
-            html = (
-                df.style
-                .use(self._styles)
-                .set_table_attributes('class="rendered_html"')
-                .render()
-            )
+            if self.tooltip is not None:
+                html = (
+                    df.style
+                    .use(self._styles)
+                    .set_tooltips(self.tooltip)
+                    .set_table_attributes('class="rendered_html"')
+                    .render()
+                )
+            else:
+                html = (
+                    df.style.use(self._styles)
+                    .set_table_attributes('class="rendered_html"')
+                    .render()
+                )
         except ValueError as e:
             # Sometimes styling fails (e.g. non-unique indexes).
             # See <https://pandas.pydata.org/pandas-docs/stable/user_guide/
@@ -96,11 +102,11 @@ class Paginator:
         return html
 
     def _update(self):
-        self.label.value = 'PAGE {}/{}'.format(self.page + 1, self.max_pages)
+        self.label.value = "PAGE {}/{}".format(self.page + 1, self.max_pages)
         self.content.value = self._render_table(self.get_page())
 
     def _handle_slider_change(self, change):
-        self.page = change['new'] - 1
+        self.page = change["new"] - 1
         self._update()
 
     def _next_page(self, b):
